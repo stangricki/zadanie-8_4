1536c6bb0f8dfa48ea797be33d0774de83ab04cb
Filip Stangricki <stangricki@gmail.com>
2017-10-24 19:24:12 +0200

    Added comments to JS file

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,19 +1,12 @@
+alert("Zadanie 8.4 - Dodajemy komentarz do skryptu")
+//alert("Zadanie 8.4 - Dodajemy komentarz do skryptu")
 
-var a = " ";
-var b = "#";
-var size = 40
-var board ="";
+// komentarz jednej linii kodu
 
-for (var y = 0; y < size; y++) {
-	for (var x = 0; x < size; x++) {
-		if ((x+y) % 2 == 0) {
-			board += a;
-		}
-		else {
-			board += b;
-		}
-	}
-	board +="\n"
-}	
+/* 
 
-console.log (board);
+komentarz 
+typu 
+"ile wlezie"
+
+*/
